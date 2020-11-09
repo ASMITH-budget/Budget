4bbeec0c793ee346d202f2d85555ce329e0b808b
dimasrw34 <boyarskidm@yandex.ru>
2020-11-09 17:03:14 +0400

    xunit tests added

---
+++ XUnitTestCore/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Budget.Core;
+
+
+namespace XUnitTestCore
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
